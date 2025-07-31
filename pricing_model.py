c3502988824596c5d1ccfa7767cc132b157d58e2
Thomas Agosti <agosti.thomas2005@gmail.com>
2025-07-31 16:19:35 +0200

    Update pricing_model.py

--- pricing_model.py
+++ pricing_model.py
@@ -1,1 +1,10 @@
 import numpy as np
+from scipy.stats import norm
+
+def black_scholes(S,K,T,r,sigma,option_type="call"):
+  d1=(np.log(S/K)+(r+0.5*sigma**2)*T)/(sigma*np.sqrt(T))
+  d2=d1-sigma*np.sqrt(T)
+  if option_type=="call":
+    return S*norm.cdf(d1)-K*np.exp(-r*T)*norm.cdf(d2)
+  else: 
+    return K*np.exp(-r*T)*norm.cdf(-d2)-S*norm.cdf(-d1)  
